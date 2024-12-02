cb2ec3ce71ffe3f9faff67b19154dbdf0be19ff0
Rushendra Kumar <rushi17092004@gmail.com>
2024-12-02 19:26:56 +0530

    Migrated from compass to atlas

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = () => {
-    mongoose.connect("mongodb://localhost:27017/UrlShortener").then(()=>{
+    mongoose.connect("mongodb+srv://rushi17092004:rushi123@cluster0.4bc8xaq.mongodb.net//UrlShortener").then(()=>{
         console.log("Connected to database");
     }).catch(e => console.log(e))
 }
